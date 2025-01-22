d76f7da8d2c37e05525d88cebac5ebb616aa9acf
TImothée Boiton <timothee.boitongmail.com>
2025-01-22 17:01:34 +0100

    Feat : CameraDezoom

--- Assets/Game/Scripts/Camera/CameraController.cs
+++ Assets/Game/Scripts/Camera/CameraController.cs
@@ -3,9 +3,18 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header("Reference")]
+    public Camera mainCamera;
+
     [Header("Settings")]
     [SerializeField] private float smoothSpeed = 5.0f;
     [SerializeField] private float distance = 10.0f;
+
+    [Header("FOV Settings")]
+    [SerializeField] private float maxFOV;
+    [SerializeField] private float minFOV;
+    [SerializeField] private float dezoomFOV;
+    [SerializeField] private float zoomSpeed;
 
     [Header("RSO")]
     [SerializeField] private RSO_InputShoot inputShoot;
@@ -29,12 +38,44 @@ public class CameraController : MonoBehaviour
         Vector3 desiredPosition = rsoPlayerTransform.Value.position + new Vector3(0, distance, 0);
 
         transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed * Time.deltaTime);
+
+        Dezoom();
+        ReZoom();
     }
 
     private void PlayCameraShake(float duration, float magnitude)
     {
         StopAllCoroutines();
         StartCoroutine(ShakeCoroutine(duration, magnitude));
+    }
+
+    private void ReZoom()
+    {
+        if (mainCamera.fieldOfView <= minFOV) 
+        { 
+            mainCamera.fieldOfView = minFOV;
+        }else
+        {
+            mainCamera.fieldOfView -= Time.deltaTime * zoomSpeed;
+        }
+    }
+
+    private void Dezoom()
+    {
+        bool canDezoom = true;
+
+        if (inputShoot.Value && canDezoom) 
+        {
+
+            Debug.Log("Has shoot");
+
+            mainCamera.fieldOfView += dezoomFOV;
+
+            if(mainCamera.fieldOfView >= maxFOV) mainCamera.fieldOfView = maxFOV;
+
+            canDezoom = false;
+        }
+        else canDezoom = true;
     }
 
     private IEnumerator ShakeCoroutine(float duration, float magnitude)
